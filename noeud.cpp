b50e57a23bd9c3f7464bfdd24fffcb6c20966813
Pauline Lavainne <pauline.lavainne@insa-lyon.fr>
2020-05-11 16:11:17 +0200

    ecriture du constructeur, du destructeur et de la fonction d'insertion

--- noeud.cpp
+++ noeud.cpp
@@ -1,13 +1,49 @@
 #include "noeud.h"
 
 //Constructor
-noeud::noeud(){
+noeud::noeud(noeud* f1, noeud* f2, noeud* m, int val){
+    fille1_ = f1;
+    fille2_ = f2;
+    mere_ = m;
+    valeur_ = val;
 
 };
 
 
 //Destructeur
 noeud::~noeud(){
+    delete this->fille1_;
+    delete this->fille2_;
+    delete this->mere_;
     
 };
+
+void noeud::insertion(int fct, noeud* nd){
+
+    //creation du nouveau noeud dans l'insertion
+    noeud new_noeud(this, nd, mere_, fct);
+    //new_noeud(this, nd, mere_, fct);
+
+    //reactulisation du noeud présent
+    *mere_ = new_noeud;
+
+};
+
+void noeud::deletion(){
+    //il faut que je trouve comment je pointe sur aucun noeud
+};
+
+void noeud::remplacement(){
+
+};
+
+bool noeud::viabilite(){
+    return true;
+};
+
+
+
+
+
+
 
--- noeud.h
+++ noeud.h
@@ -2,16 +2,24 @@ class noeud{
 
 public:
 //Constructor
-	noeud ();
+	noeud (noeud* f1, noeud* f2, noeud* m, int val);
 
 //Destructor
 	~noeud ();
 
 
 //Fonctions
-		
+	void insertion(int fct, noeud* nd);
+    void deletion();
+    void remplacement();
+    bool viabilite();
+
 
 protected:
 
+    noeud* fille1_;
+    noeud* fille2_;
+    noeud* mere_;
+    int valeur_;
 	
 };
