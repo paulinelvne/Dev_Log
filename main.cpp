17daf433339749ce5b50d1d16ca01f6b0e4d14f1
Nino155 <62557495+Nino155@users.noreply.github.com>
2020-06-14 16:53:30 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,6 +2,38 @@
 #include<iostream>
 
 int main(){
+    // test fonction rendu :
+
+    std::vector<int> vect1(1,1);
+    std::vector<int> vect2(0,0);
+    std::vector<int> vect3(3,3);
+    noeud* noeud1 = new noeud(NULL, NULL, NULL , vect1, 133);
+    noeud* noeud2 = new noeud(NULL, NULL, NULL, vect2, 134);
+    noeud* noeud3 = new noeud(noeud1, noeud2, NULL, vect3, 140);
+    noeud3->put_str("OR");
+    noeud2->put_str("x1");
+    noeud1->put_str("x2");
+    noeud1->put_mere(noeud3);
+    noeud2->put_mere(noeud3);
+    vector<noeud*> content = {noeud1, noeud2, noeud3};
+    formule* f1 = new formule(content, 1200);
+
+    fonctionnement* ff = new fonctionnement(content);
+
+
+    noeud* noeud1b = new noeud(NULL, NULL, NULL , vect1, 133);
+    noeud* noeud2b = new noeud(NULL, NULL, NULL, vect2, 134);
+    noeud* noeud3b = new noeud(noeud1b, noeud2b, NULL, vect3, 140);
+    noeud3b->put_str("OR");
+    noeud2b->put_str("x1");
+    noeud1b->put_str("x2");
+    noeud1b->put_mere(noeud3b);
+    noeud2b->put_mere(noeud3b);
+    vector<noeud*> content2 = {noeud1, noeud2, noeud3};
+    formule* f2 = new formule(content, 1250);
+    vector<formule*> mutant = {f1,f2};
+    rendu(ff->selection(mutant));
+/*
 
     std::vector<int> vect1(4,4);
     std::vector<int> vect2(5,5);
@@ -112,5 +144,6 @@ int main(){
     // delete noeud1;
     // delete noeud2;
     // delete noeud3;
-
+*/
+  
 } 
